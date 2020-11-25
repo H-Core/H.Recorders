e39b202cfcbb20cff53dfbdf8d9b80f5d0d81956
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-25 12:34:07 +1000

    Added HomeCenter recorders with tests.

---
+++ src/libs/H.Recorders.WinmmRecorder/StreamEmulatorRecorder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using H.Core;
+using H.Core.Attributes;
+using H.Core.Recorders;
+
+namespace H.Recorders
+{
+    [AllowMultipleInstance(false)]
+    public class StreamEmulatorRecorder : ParentRecorder
+    {
+        #region Properties
+
+        public int Interval { get; }
+        public Timer Timer { get; }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<VoiceActionsEventArgs>? NewPartialData;
+        private void OnNewPartialData() => NewPartialData?.Invoke(this, new VoiceActionsEventArgs
+        {
+            Data = RawData ?? WavData,
+        });
+
+        #endregion
+
+        #region Constructors
+
+        public StreamEmulatorRecorder(IRecorder recorder, int interval)
+        {
+            Recorder = recorder;
+            Interval = interval;
+            
+            Timer = new Timer(OnTimer, null, 0, Interval);
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private async void OnTimer(object sender)
+        {
+            if (!IsStarted)
+            {
+                return;
+            } 
+
+            await StopAsync().ConfigureAwait(false);
+            OnNewPartialData();
+            //File.WriteAllBytes($"D:/voice_{new Random().Next()}.wav", Data);
+            await StartAsync().ConfigureAwait(false);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public override void Dispose()
+        {
+            Timer.Dispose();
+
+            base.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
---
+++ src/libs/H.Recorders.WinmmRecorder/WinmmRecorder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core.Recorders;
+
+namespace H.Recorders
+{
+    public class WinmmRecorder : Recorder
+    {
+        #region Private methods
+
+        // Winmm.dll is used for recording speech
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Unicode)]
+        private static extern int MciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        private static void MciSendString(string command)
+        {
+            var result = MciSendString(command, "", 0, 0);
+            if (result != 0)
+            {
+                throw new InvalidOperationException($"MciSendString returns bad result: {result}");
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public override async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            MciSendString("open new Type waveaudio Alias recsound");
+            MciSendString("record recsound");
+
+            await base.StartAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken = default)
+        {
+            var path = Path.GetTempFileName();
+            MciSendString("save recsound " + path);
+            MciSendString("close recsound ");
+
+            if (File.Exists(path))
+            {
+                try
+                {
+                    WavData = File.ReadAllBytes(path);
+                }
+                finally
+                {
+                    File.Delete(path);
+                }
+            }
+
+            await base.StopAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
